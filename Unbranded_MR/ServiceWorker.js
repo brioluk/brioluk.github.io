5e992fe662d7668f9bea6ae0e3e8120549de718f
Brioluk <brioluk@gmail.com>
2023-10-22 12:11:04 +0200

    Unbranded MR

---
+++ Unbranded_MR/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-NF_SneakerXR2022_WebGL_MRTK-0.1";
+const contentToCache = [
+    "Build/v01.loader.js",
+    "Build/v01.framework.js.unityweb",
+    "Build/v01.data.unityweb",
+    "Build/v01.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
